ad2946e3a4df1aa9838dd3c9c1c4c61138c5b08c
valenn0101 <veroncaceresvalentin@gmail.com>
2023-06-14 22:49:52 -0300

    The model of the airplanes in the technical challenge
    he idea later is to use these models to generate a composite object consisting of three others, which will contain arrays of each group and set of seats.

---
+++ src/models/AirplanesModels.ts
@@ -0,0 +1,66 @@
+const airplane1 = {
+  type: "AirNova-660",
+  classes: [
+    {
+      name: "Primera Clase",
+      seatType: 1,
+      groups: [
+        { rows: 2, columns: 4 },
+        { rows: 2, columns: 4 }
+      ]
+    },
+    {
+      name: "Económica Premium",
+      seatType: 2,
+      groups: [
+        { rows: 3, columns: 8 },
+        { rows: 3, columns: 8 }
+      ]
+    },
+    {
+      name: "Clase Económica 3",
+      seatType: 3,
+      groups: [
+        { rows: 3, columns: 16 },
+        { rows: 3, columns: 16 }
+      ]
+    }
+  ],
+  totalSeats: 160
+};
+
+const airplane2 = {
+  type: "AirMax-720neo",
+  classes: [
+    {
+      name: "Primera Clase",
+      seatType: 1,
+      groups: [
+        { rows: 1, columns: 5 },
+        { rows: 1, columns: 5 },
+        { rows: 1, columns: 5 }
+      ]
+    },
+    {
+      name: "Económica Premium",
+      seatType: 2,
+      groups: [
+        { rows: 2, columns: 6 },
+        { rows: 3, columns: 6 },
+        { rows: 2, columns: 6 }
+      ]
+    },
+    {
+      name: "Clase Económica",
+      seatType: 3,
+      groups: [
+        { rows: 2, columns: 14 },
+        { rows: 3, columns: 14 },
+        { rows: 2, columns: 14 }
+      ]
+    }
+  ],
+  totalSeats: 155
+};
+
+export { airplane1, airplane2 };
